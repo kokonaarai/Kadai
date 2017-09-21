ea4c7454e6067a00e90641c4edc706257454bb2c
Kokona Arai <coconachanx3@gmail.com>
2017-09-21 13:26:33 +0900

    java クエリストリングの利用　再提出

--- CampChallenge_java/016.データ操作/QS_jsp.java
+++ CampChallenge_java/016.データ操作/QS_jsp.java
@@ -51,6 +51,35 @@ public final class QS_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("        <title>JSP Page</title>\n");
       out.write("    </head>\n");
       out.write("    <body>\n");
+      out.write("        \n");
+      out.write("        ");
+
+    request.setCharacterEncoding("UTF-8");
+    int t = Integer.parseInt(request.getParameter("total"));
+    int c = Integer.parseInt(request.getParameter("count"));
+    int ty = Integer.parseInt(request.getParameter("type"));
+    out.print("総額" + request.getParameter("total") + "円<br>");
+    if(ty ==1){
+    out.print("雑貨<br>");
+    }
+    else if(ty ==2){
+    out.print("生鮮食品<br>");
+    }
+    else if(ty ==3){
+    out.print("その他<br>");
+    }
+    out.print("個数" + request.getParameter("count") + "個(１個あたり" + t / c + "円）<br><br>");
+    if (t > 4999) {
+        double p = t * 0.05;
+        out.print("5000円以上" + p + "ポイント");
+    } else if (t > 2999) {
+        double p = t * 0.04;
+        out.print("3000円以上" + p + "ポイント");
+    }
+
+      out.write("\n");
+      out.write("        \n");
+      out.write("        \n");
       out.write("        <h1> 1.</h1>\n");
       out.write("        typeが１だったら、「雑貨」<br>\n");
       out.write("        typeが２だったら、「生鮮食品」<br>\n");
@@ -59,17 +88,16 @@ public final class QS_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("        <h1> 2.</h1>\n");
       out.write("        ");
  int i = 1500/10;
-        System.out.print("一個あたりの値段は"+i+"です");
-        
-      out.write("\n");
-      out.write("        \n");
-      out.write("        <h1>3.</h1>\n");
-      out.write("        ");
+        out.print("一個あたりの値段は"+i+"です");
 
-            double a = 3000*0.04;
-            System.out.println("3000円以上で"+a+"ポイント");
+      out.write("\n");
+      out.write("\n");
+      out.write("<h1>3.</h1>\n");
+
+     double a= 3000*0.04;     
+           out.print("3000円以上で"+a+"ポイント"+"<br>");
             double j = 5000*0.05;
-            System.out.print("5000円以上"+j+"ポイント");
+            out.print("5000円以上"+j+"ポイント");
             
            
             
